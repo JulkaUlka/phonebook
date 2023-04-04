ae64e5fc20c932d28f11775144ff72b8bfa685f1
JulkaUlka <y.glushko@outlook.com>
2023-04-05 01:28:47 +0300

    added container

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
+import { Container } from 'components/App.styled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter basename='/goit-react-hw-08-phonebook/'>
+          <Container>
           <App />
+          </Container>
+          
         </BrowserRouter>
       </PersistGate>
     </Provider>
